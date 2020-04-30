f7bf695947d8ce48274ffbc06c168c028a818597
lhack-jpeg <62835319+lhack-jpeg@users.noreply.github.com>
2020-04-30 13:58:10 +1000

    Add files via upload
    basic number checker

---
+++ basic odd or even checker.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 13:53:08 2020
+
+@author: luke-
+"""
+
+number = int(input('please enter a number:'))
+if number%2 == 0:
+    print("that number is even")
+else:
+    print("that number is odd")
